6e772ba8db46f48489884fc9436095b1292a5bf8
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-04-04 20:46:22 +0530

    Initial Commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = Join-Path "$(Get-ToolsLocation)" "PolyMC"
+$url        = 'https://packages.polymc.org/1.0.6/win32/win32-1.0.6.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $installDir
+  url           = $url
+  softwareName  = 'PolyMC*'
+  checksum      = '06e7d942a5d88dfd128f8c8c8f32f6d29d46a8df612e4f5402684f9e0b5f0de9'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Host "Creating Start Menu and Desktop shortcuts"
+$startmenu = Join-Path $env:programdata "Microsoft\Windows\Start Menu\Programs"
+Install-ChocolateyShortcut -shortcutFilePath $(Join-Path $startmenu "PolyMC.lnk") -TargetPath $(Join-Path $installDir "PolyMC.exe")
+$desktop = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyShortcut -shortcutFilePath $(Join-Path $desktop "PolyMC.lnk") -TargetPath $(Join-Path $installDir "PolyMC.exe")
