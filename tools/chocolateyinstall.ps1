ca8e875337630e386393e139570f47beb7687870
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-05-30 01:10:58 +0530

    chore: Bump to 1.3.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = Join-Path "$(Get-ToolsLocation)" "PolyMC"
-$url        = 'https://github.com/PolyMC/PolyMC/releases/download/1.3.0/PolyMC-Windows-x86_64-1.3.0.zip'
+$url        = 'https://github.com/PolyMC/PolyMC/releases/download/1.3.1/PolyMC-Windows-x86_64-1.3.1.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
   url           = $url
   softwareName  = 'PolyMC*'
-  checksum      = 'cb11189b7185d9395ff9c9676f78c0d2c4a48b489fcd7e90e4198608a64efab4'
+  checksum      = '14da2bcee2053a9c751add8dd11cf1ac85942f8822f7f7749bc56bc358afe970'
   checksumType  = 'sha256'
 }
 
