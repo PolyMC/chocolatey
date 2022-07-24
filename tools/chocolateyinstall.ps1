24a551e1eed05a40b2b534469acfb5d7941a953b
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-07-24 20:42:37 +0530

    feat: Use Get-PackageParameters to allow... specifying standard and legacy versions to install
    Standard installation: `choco install polymc`
    Legacy installation: `choco install polymc --params='/legacy'`
    
    Signed-off-by: txtsd <thexerothermicsclerodermoid@gmail.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$pp         = Get-PackageParameters
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = Join-Path "$(Get-ToolsLocation)" "PolyMC"
-$url        = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.0/PolyMC-Windows-1.4.0.zip'
+
+# Read parameter to decide if we install standard or legacy
+if (!$pp['legacy']) {
+  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.0/PolyMC-Windows-1.4.0.zip'
+  $checksum = '89da603458deb29c3e9ad71801fb679d7b06ba1937d305b7a27360bde1df6670'
+}
+else {
+  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.0/PolyMC-Windows-Legacy-1.4.0.zip'
+  $checksum = '4f52c6b002a2d33027ac5e51b5b91f42e6460473834a7b701d7740680871c57c'
+}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
   url           = $url
   softwareName  = 'PolyMC*'
-  checksum      = '89da603458deb29c3e9ad71801fb679d7b06ba1937d305b7a27360bde1df6670'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
