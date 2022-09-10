b2d5e0e0d9c874b67c663f1272b7268040d4d736
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-09-10 13:37:18 +0530

    chore: Bump to 1.4.2
    Signed-off-by: txtsd <thexerothermicsclerodermoid@gmail.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $installDir = Join-Path "$(Get-ToolsLocation)" "PolyMC"
 
 # Read parameter to decide if we install standard or legacy
 if (!$pp['legacy']) {
-  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.1/PolyMC-Windows-1.4.1.zip'
-  $checksum = '702d833313318fe8e3dbafc9540d0db23b1a4c6f682283aaa6b302886c5f1aee'
+  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.2/PolyMC-Windows-1.4.2.zip'
+  $checksum = '368b17560fd9571483b5915ba56b155f06b3963c7e6371fc8eb0cef1a3eb18ee'
 }
 else {
-  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.1/PolyMC-Windows-Legacy-1.4.1.zip'
-  $checksum = '71268c2d508446c758fedc6a8c96e94de2e8309a13693711d79d6d6a02dcc2c0'
+  $url      = 'https://github.com/PolyMC/PolyMC/releases/download/1.4.2/PolyMC-Windows-Legacy-1.4.2.zip'
+  $checksum = '03facfa98a8e26290e7a597a8f4876a06474e0217c344f0ec4015f630df81e64'
 }
 
 $packageArgs = @{
